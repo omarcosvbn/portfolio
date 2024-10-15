548bb5f4237053a52d5ab2eb9b84e4fbfc166b9d
omarcosvbn <omarcosvbn@gmail.com>
2024-10-15 18:26:14 +0100

    framer-motion

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,26 +1,156 @@
 import Theme from "../../components/Theme";
 import style from "./Homepage.module.scss";
 import Typewriter from "typewriter-effect";
+import { motion } from "framer-motion";
 
 export default function Homepage() {
   return (
     <>
       <Theme />
-      <div>
-        <div className={style.type}>
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("Marcos")
-                .start();
-            }}
-          />
-        </div>
-        <img src="/assets/arrow.svg" alt="" width="100px" className={style.arrow}/>
-        <p>(scroll just a bit more)</p>
+
+      <div className={style.type}>
+        <motion.div 
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }} 
+          transition={{ type: "spring" }}  
+        />
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString("Marcos").start();
+          }}
+        />
       </div>
+
+      <img
+        src="/assets/arrow.svg"
+        alt=""
+        width="100px"
+        className={style.arrow}
+      />
+      <p>(scroll just a bit more)</p>
       <div>
         <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+        <h2>projects</h2>
+
       </div>
     </>
   );
