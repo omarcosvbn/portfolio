6b1305ca46a5a533d18deab8d378962ac0d16efa
omarcosvbn <omarcosvbn@gmail.com>
2024-10-22 16:03:03 +0100

    New font and Skills

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import "../styles/Home.module.scss";
 
-
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <head>
         <title>Portfolio</title>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
       </head>
       <body>{children}</body>
     </html>
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,9 +1,42 @@
 import Overlay from "../../components/Overlay/Overlay";
+import style from "./Skills.module.scss";
 
 export default function Skills() {
   return (
     <>
       <Overlay />
+      <div className={style.container}>
+
+        <div className={style.container__images}>
+          <div className={style.container__skill}>
+            <i className="devicon-html5-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-css3-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-javascript-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-typescript-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-nextjs-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-postgresql-plain" />
+          </div>
+        </div>
+
+        <div className={style.container__images}>
+          <div className={style.container__skill}>
+            <i className="devicon-unity-plain" />
+          </div>
+          <div className={style.container__skill}>
+            <i className="devicon-godot-plain" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
