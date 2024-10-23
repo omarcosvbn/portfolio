6c3cce98a5496e724254c09b7d40beac65c5c402
omarcosvbn <omarcosvbn@gmail.com>
2024-10-23 04:53:11 +0100

    Fix link contacts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,17 +4,20 @@ export default function About() {
   return (
     <>
       <div className={style.text}>
-        <p>
-          Hello, I’m Marcos, I develop{" "}
-          <span className={style.span}>Front-End</span> and{" "}
-          <span className={style.span}>games</span>.
-        </p>
-        <p>
-          Graduated in <span className={style.span}>Design and Multimedia</span>
-          , I apply my design knowledge in every project that I make, from{" "}
-          <span className={style.span}>cute buttons</span> to{" "}
-          <span className={style.span}>user-centered design</span>.
-        </p>
+        <div className={style.box}>
+          <p>
+            Hello, I’m Marcos, I develop{" "}
+            <span className={style.span}>Front-End</span> and{" "}
+            <span className={style.span}>games</span>.
+          </p>
+          <br/>
+          <p>
+            I am graduated in{" "}
+            <span className={style.span}>Design and Multimedia</span>, which allows me to apply design knowledge in every project that I make, from{" "}
+            <span className={style.span}>cute buttons</span> to{" "}
+            <span className={style.span}>user-centered design</span>.
+          </p>
+        </div>
       </div>
     </>
   );
--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import style from "./Contacts.module.scss";
+import Link from "next/link";
+
 export default function Contacts() {
   return (
-    <>
-      <h1>a</h1>
-    </>
+    <div className={style.links}>
+      <Link href="mailto:john@example.com"><Image src="/images/email.png" alt="Email" width="100" height="100" className={style.links__image} /></Link>
+      <Link href="https://johnexample.com"><Image src="/images/linkedin.png" alt="LinkedIn" width="100" height="100" className={style.links__image} /></Link>
+
+    </div>
   );
 }
