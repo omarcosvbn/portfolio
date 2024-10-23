a398f7538a3325e418e8241b2d9112be72a661e9
omarcosvbn <omarcosvbn@gmail.com>
2024-10-23 23:40:34 +0100

    Code Homepage animations

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,13 +1,41 @@
 import Image from "next/image";
 import style from "./Contacts.module.scss";
-import Link from "next/link";
 
 export default function Contacts() {
   return (
     <div className={style.links}>
-      <Link href="mailto:john@example.com"><Image src="/images/email.png" alt="Email" width="100" height="100" className={style.links__image} /></Link>
-      <Link href="https://johnexample.com"><Image src="/images/linkedin.png" alt="LinkedIn" width="100" height="100" className={style.links__image} /></Link>
-
+      <div className={style.links__contacts}>
+        <a
+          href="mailto:omarcosvbn@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className={style.links__image}
+            src="/images/email.png"
+            alt="Email"
+            width={100}
+            height={100}
+          />
+        </a>
+        <h2>omarcosvbn@gmail.com</h2>
+      </div>
+      <div className={style.links__contacts}>
+        <a
+          href="https://www.linkedin.com/in/marcos-bnascimento/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className={style.links__image}
+            src="/images/linkedin.png"
+            alt="LinkedIn"
+            width={100}
+            height={100}
+          />
+        </a>
+        <h2>marcos-bnascimento</h2>
+      </div>
     </div>
   );
 }
