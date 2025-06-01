13fc24c873cc9557224eb016e1034bd91c32586a
omarcosvbn <omarcosvbn@gmail.com>
2025-06-01 19:01:37 +0100

    Base home content displacement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
 import HomeButton from "./components/HomeButton/HomeButton";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.home}>
-        <h1 className={styles.home__title}>Hi, I am Marcos</h1>
-        <HomeButton
-          label="projects"
-          link1="case studies"
-          link2="personal"
-          setLink="/projects"
-        />
-        <HomeButton label="contacts" link1="linkedin" link2="email" />
+        <div className={styles.home__hi}>
+          <div className={styles.home__hi__title}>
+            <h1>Hi,</h1>
+            <h1>I</h1>
+            <h1>am</h1>
+            <h1>Marcos</h1>
+          </div>
+          <HomeButton
+            label="projects"
+            link1="case studies"
+            link2="personal"
+            setLink="/projects"
+          />
+          <HomeButton label="contacts" link1="linkedin" link2="email" />
+        </div>
         <div className={styles.home__about}>
           <p className={styles.home__about__text}>about me</p>
-          <div className={styles.home__about__arrow}>seta</div>
+          <p className={styles.home__about__arrow}>seta</p>
         </div>
       </div>
     </div>
