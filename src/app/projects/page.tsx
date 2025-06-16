8666269e172eed3bbfe277251b6da4e163461e7c
omarcosvbn <omarcosvbn@gmail.com>
2025-06-16 21:24:07 +0100

    Projects json created and fetched in projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,38 @@
-"use client"
+"use client";
 
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type ProjectData = {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  technologies: string;
+  type: string;
+};
 
 export default function ProjectsPage() {
   const searchParams = useSearchParams();
   const filter = searchParams.get("filter");
+  const [projects, setProjects] = useState<ProjectData[]>([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then(setProjects);
+  }, []);
+
+  if (projects.length === 0) return <p>Loading...</p>;
 
   return (
     <div>
       <h1>Projects</h1>
-      {filter === "personal" && <></>}
+      {filter === "personal" && (
+        <>
+          <p>{projects[0].title}</p>
+        </>
+      )}
       {filter === "case-studies" && <></>}
       {!filter && <></>}
     </div>
