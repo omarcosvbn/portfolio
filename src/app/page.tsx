885384f6bd73db28a2ced5e8ad4c0c17f708bc22
omarcosvbn <omarcosvbn@gmail.com>
2025-10-16 01:25:34 +0100

    hi, i am marcos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,17 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div>
-        <Link href={"/projects"}>projects</Link>
-        <a href="https://www.linkedin.com/in/marcos-bnascimento/">linkedin</a>
-        <a href="https://github.com/omarcosvbn">github</a>
+      <div className={styles.mainPage}>
+        <div className={styles.hello}>
+          {Array.from({ length: 15 }).map((_, i) => (
+            <img src="https://placehold.co/400x700" className={styles.image} />
+          ))}
+        </div>
+        <div className={styles.links}>
+          <Link href={"/projects"}>projects</Link>
+          <a href="https://www.linkedin.com/in/marcos-bnascimento/">linkedin</a>
+          <a href="https://github.com/omarcosvbn">github</a>
+        </div>
       </div>
     </div>
   );
